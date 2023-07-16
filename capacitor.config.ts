5492b33265ce71ce2cc915a94205829d9f93f336
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2023-07-16 11:15:46 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'looking-menu',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
